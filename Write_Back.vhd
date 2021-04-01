73aa451ae64c61c8a6e1613fbd118da4cc1761e1
Anas Deis <anas.deis@mail.mcgill.ca>
2021-03-31 20:37:01 -0400

    Added Instruction_Memory

--- Write_Back.vhd
+++ Write_Back.vhd
@@ -11,24 +11,24 @@ use ieee.numeric_std.all;
 entity Write_Back is
 port (
 	mem_op_en_in: in std_logic;					    -- enable using result from MEM (lw,sw), otherwise use result from EX
-	register_file_en_in: in std_logic;				-- enable writing to register_file.txt if opcode is not a branch or jump
-	EX_in : in std_logic_vector (31 downto 0);		-- result from EX
-	MEM_in: in std_logic_vector (31 downto 0);		-- result from MEM
-	IR_in: in std_logic_vector (4 downto 0);		-- instruction in
+	register_file_en_in: in std_logic;				 -- enable writing to register_file.txt if opcode is not a branch or jump
+	EX_in : in std_logic_vector (31 downto 0);	 -- result from EX
+	MEM_in: in std_logic_vector (31 downto 0);	 -- result from MEM
+	IR_in: in std_logic_vector (4 downto 0);		 -- instruction in
 	
-	register_file_en_out: out std_logic;			-- enable writing to register_file.txt if opcode is not a branch or jump
-	MUX_out : out std_logic_vector (31 downto 0);	-- data to write back
-	IR_out: out std_logic_vector (4 downto 0)		-- instruction out
+	register_file_en_out: out std_logic;			 -- enable writing to register_file.txt if opcode is not a branch or jump
+	MUX_out : out std_logic_vector (31 downto 0); -- data to write back
+	IR_out: out std_logic_vector (4 downto 0)		 -- instruction out
   );
 end wb;
 
 architecture behavioral of Write_Back is
 
 begin
-	process(EX_in, MEM_in, mem_op_en_in, register_file_en_out)
+	process(mem_op_en_in, EX_in, MEM_in, register_file_en_out)
 	begin
 		register_file_en_out <= register_file_en_in;	 -- enable writing
-		IR_OUT <= IR_IN;								 -- destination register address
+		IR_OUT <= IR_IN;								       -- destination register address
 		
 		-- check opcode if (lw,sw) or otherwise
 		case mem_op_en_in is
@@ -38,6 +38,7 @@ begin
 			-- use result from MEM stage
 			when '1' => 
 				MUX_out <= MEM_in;
+			-- else unknown
 			when others => 
 				MUX_out <= x"XXXXXXXX"; 
 		end case;
