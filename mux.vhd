b0fa4a48c0fac2f73a3d30288426988b945eeba6
Albert Assouad <36460811+albertassouad@users.noreply.github.com>
2021-03-31 20:12:34 -0400

    add mux pc and adder
    components to be used in pipeline

---
+++ mux.vhd
@@ -0,0 +1,18 @@
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+entity mux is
+    Port ( select : in  STD_LOGIC;
+           input_0   : in  STD_LOGIC_VECTOR (31 downto 0);
+           input_1   : in  STD_LOGIC_VECTOR (31 downto 0);
+           output   : out STD_LOGIC_VECTOR (31 downto 0));
+end mux;
+
+architecture mux_arch of mux is
+
+begin
+  
+    output <= input_1 when (SEL = '1') else input_0;
+
+end mux_arch;
+
