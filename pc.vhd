835efacfb4cd4941ce488f7825e3d9daddc70605
Albert Assouad <albert.assouad@mail.mcgill.ca>
2021-03-31 18:26:38 -0700

    Update pc.vhd

--- pc.vhd
+++ pc.vhd
@@ -4,9 +4,9 @@ use IEEE.numeric_std.all;
 
 entity pc is
 port(clk : in std_logic;
-	 reset : in std_logic;
-	 counterOutput : out std_logic_vector(31 downto 0);
-	 counterInput : in std_logic_vector(31 downto 0) := x"00000000"
+	 rst : in std_logic;
+	 pcOutput : out std_logic_vector(31 downto 0);
+	 pcInput : in std_logic_vector(31 downto 0) := x"00000000"
 	 );
 end pc;
 
@@ -15,13 +15,13 @@ architecture pc_arch of pc is
  
 begin
 
-process (clk,reset)
+process (clk,rst)
 begin
 	
-	if (reset = '1') then -- set PC to first instruction
-		counterOutput <= x"00000000";
+	if (rst = '1') then -- set PC to first instruction
+		pcOutput <= x"00000000";
 	elsif (clk'event and clk = '1') then 	-- set PC to next instruction
-		counterOutput <= counterInput;
+		pcOutput <= pcInput;
 
 	end if;
 	
